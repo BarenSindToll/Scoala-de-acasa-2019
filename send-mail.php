28e4b0ed767cad4a168f555045ee937f2cfca8a8
AnaVit <anamariavit29@gmail.com>
2020-05-12 12:35:17 +0300

    php

--- send-mail.php
+++ send-mail.php
@@ -1,35 +1,29 @@
+<script src = js/main.js></script>
 <?php
-    $mail_to = 'anamariavit29@gmail.com'; // specify your email here
+    $metoda = $_SERVER['REQUEST_METHOD']
+    echo "Metoda invocata este: $metoda <br>";
+    if($metoda == "POST")
+    {
+      if(isset($_POST['Trimite'])&&empty($_SESSION['valoare']))
+      {
+        $varNume=$_POST['Nume'];
+        $varEMail=$_POST['Email'];
+        $varMesaj=$_POST['Mesaj'];
+        $servername="192.168.0.4";
+        $username="avit";
 
-    // Assigning data from the $_POST array to variables
-    $name = $_POST['sender_name'];
-    $mail_from = $_POST['sender_email'];
-    $message = $_POST['sender_message'];
+      }
+      $conn = new mysql($servername,$username,$dbname);
+      if($conn->connect_error) {
+        die("Connection failed:".$conn->connect_error);
+      }
+      $sql = "INSERT INTO PCONTACT(Numr, Email, Mesaj)
+      VALUES('$varNume','$varEMail', '$varMesaj')";
 
-    // Construct email subject
-    $subject = 'Scoala de Acasa Message from visitor ' . $name;
-
-    // Construct email body
-    $body_message = 'From: ' . $name . "\r\n";
-    $body_message .= 'E-mail: ' . $mail_from . "\r\n";
-    $body_message .= 'Message: ' . $message;
-
-    // Construct email headers
-    $headers = 'From: ' . $mail_from . "\r\n";
-    $headers .= 'Reply-To: ' . $mail_from . "\r\n";
-
-    $mail_sent = mail($mail_to, $subject, $body_message, $headers);
-
-    if ($mail_sent == true){ ?>
-        <script language="javascript" type="text/javascript">
-        alert('Thank you for the message. We will contact you shortly.');
-        window.location = '/contact.html';
-        </script>
-    <?php } else { ?>
-    <script language="javascript" type="text/javascript">
-        alert('Message not sent. Please, notify the site administrator admin@admin.com');
-        window.location = '/contact.html';
-    </script>
-    <?php
-    }
+      if($conn->query($sql) === TRUE){
+        eco "New record created successfully";
+      }
+      else{
+        echo "Error:".$sql."<br>". $conn->error;
+      }
 ?>
